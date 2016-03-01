7ff93fac2ada06993620ed9bc563e136427dbe2e
Tomso93 <Strnka.Tomas@gmail.com>
2016-03-01 16:34:34 +0100

    Už je čas...

---
+++ xqr.php
@@ -0,0 +1,19 @@
+<<?php
+#XQR:xstrnk01
+
+/*
+ *
+ * autor: xstrnk01
+ * email: xstrnk01@stud.fit.vutbr.cz
+ * predmet: Principy programovacich jazyku a OOP
+ * projekt: XQR->XML Query
+ * vytvoreno: 28.2.1015
+ * verze: 1.00
+ * verze php: 5
+ *
+ */
+ function help(){
+ }
+
+ 
+ ?>
